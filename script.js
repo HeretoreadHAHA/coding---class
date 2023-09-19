cb350f4f6ff2167074d2f2a3604f9f3cef72483d
HeretoreadHAHA <pyakurelbaibhav1@gmail.com>
2023-09-19 17:32:30 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// function count(sentence,countInput){
+// let sentenceArray=sentence.split()
+// console.log(sentenceArray);
+// for (i=0;i<sentence.length;i++){
+//     if (sentenceArray[i]= countInput){
+//         count = count+1;
+//     }
+// }
+// return count;
+// }
+// let a = count("my name is baibhav my surname is pyakurel the","the")
+// console.log(a);
+
